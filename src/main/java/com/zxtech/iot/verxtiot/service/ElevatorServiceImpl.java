c6e4ec433231728ae8dd721b13ddf5d3f7da3a73
Allen Wang <allenair@gmail.com>
2018-03-21 14:32:46 +0800

    c

--- src/main/java/com/zxtech/iot/verxtiot/service/ElevatorServiceImpl.java
+++ src/main/java/com/zxtech/iot/verxtiot/service/ElevatorServiceImpl.java
@@ -68,16 +68,14 @@ public class ElevatorServiceImpl {
 	public void handler(TransferElevatorParameter parameter){
 		JsonObject analyBean = IotParseTools.getAnalysisElBean(parameter);
 
-//		Completable coldb = elevatorDao.insertCollectDb(parameter);
-//		Completable analydb = elevatorDao.insertAnalysisDb(analyBean);
-//		coldb.andThen(analydb).subscribe(() -> {
-//			dealData(analyBean);
-//		}, err -> {
-//			logger.error("EL:{}, Some Errors happen {}", parameter.getElevatorId(), err.getMessage());
-//		});
-		
-		logger.info("EL=Data>> "+parameter.toJson().encode());
-		dealData(analyBean);
+		Completable coldb = elevatorDao.insertCollectDb(parameter);
+		Completable analydb = elevatorDao.insertAnalysisDb(analyBean);
+		coldb.andThen(analydb).subscribe(() -> {
+			dealData(analyBean);
+			
+		}, err -> {
+			logger.error("EL:{}, Some Errors happen {}", parameter.getElevatorId(), err.getMessage());
+		});
 	}
 	
 	private void dealData(JsonObject analyBean) {
